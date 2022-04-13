83c3125fecdd1cecdbbcf932c135116d5a8c3796
Reinkuru <20104102@ittelkom-pwt.ac.id>
2022-04-13 20:54:42 +0700

    fitness

--- scripts/animate_header.js
+++ scripts/animate_header.js
@@ -2,3 +2,8 @@ window.addEventListener ("scroll", function () {
     var header = document.querySelector ("header");
     header.classList.toggle ("sticky", window.scrollY > 0);
 });
+
+window.addEventListener ("scroll", function () {
+    var header = document.querySelector ("nav");
+    header.classList.toggle ("sticky-nav", window.scrollY > 0);
+});
